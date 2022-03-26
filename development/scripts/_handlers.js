cd5547a9ee35effe741ad649ac205759b18b437e
Anton Shchukin <a.a.shchukin@gmail.com>
2022-03-27 00:46:11 +0700

    consonants heading

--- development/scripts/_handlers.js
+++ development/scripts/_handlers.js
@@ -1,59 +1,6 @@
 (function($) {
 
 
-
-    $('input[type="checkbox"]').change(function(e) {
-
-        var checked = $(this).prop("checked"),
-            container = $(this).parents('.checklist__column'),
-            siblings = container.siblings();
-
-        container.find('input[type="checkbox"]').prop({
-            indeterminate: false,
-            checked: checked
-        });
-
-        function checkSiblings(el) {
-
-            var parent = el.parent().parent(),
-                all = true;
-
-            el.siblings().each(function() {
-                let returnValue = all = ($(this).find('input[type="checkbox"]').prop("checked") === checked);
-                return returnValue;
-            });
-
-            if (all && checked) {
-
-                parent.find('input[type="checkbox"]').prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-
-                checkSiblings(parent);
-
-            } else if (all && !checked) {
-
-                parent.find('input[type="checkbox"]').prop("checked", checked);
-                parent.find('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
-                checkSiblings(parent);
-
-            } else {
-
-                el.parents("li").find('input[type="checkbox"]').prop({
-                    indeterminate: true,
-                    checked: false
-                });
-
-            }
-
-        }
-
-        checkSiblings(container);
-    });
-
-
-
     /* vowels */
 
     function vowels() {
