4e632ff571f635e092718efa76e0dbad9fd1c030
Anton Shchukin <a.a.shchukin@gmail.com>
2022-09-10 14:46:16 +0700

    fetch based cache

--- development/scripts/_sound.js
+++ development/scripts/_sound.js
@@ -4,15 +4,38 @@
 
     $('[data-audio]').on('click', function (event) {
         event.stopPropagation(); /* Если кликнули на data-audio внутри data-audio: */
-        if( ! isPlaying ) { /* Чтобы не запустить проигрывание звуков оновременно */
+        if (!isPlaying) { /* Чтобы не запустить проигрывание звуков оновременно */
             isPlaying = true;
             const audio = new Audio($(this).data('audio'));
             audio.play();
-            audio.onended = function() {
+            audio.onended = function () {
                 isPlaying = false;
             };
         }
         /* delete object? */
+    });
+
+
+    $(window).on('load', function () {
+
+        var playlist = [];
+
+        $('[data-audio]').each(function () {
+            if (!playlist.includes($(this).data('audio'))) {
+                playlist.push($(this).data('audio'));
+            }
+        });
+
+        console.log('Downloading media:\n');
+
+        playlist.forEach(function (item) {
+            fetch('http://thai-alphabet.com/' + item)
+                .then((response) => {
+                    console.log(response.url, response.statusText)
+                });
+
+        });
+
     });
 
 
